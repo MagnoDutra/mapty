88a7c7c6d1a5577385c3c5fc70fec47687047ce5
Jose Dutra <jose.dutra@sinqia.com.br>
2023-07-26 01:51:42 -0300

    Click on map create mark feature

--- script.js
+++ script.js
@@ -16,19 +16,32 @@ if (navigator.geolocation) {
     function (pos) {
       const { latitude } = pos.coords;
       const { longitude } = pos.coords;
-      const link = `https://www.google.com.br/maps/@${latitude},${longitude}`;
+      const coords = [latitude, longitude];
 
-      const map = L.map('map').setView([51.505, -0.09], 13);
+      const map = L.map('map').setView(coords, 17);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([51.5, -0.09])
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Não foi possível pegar sua localização!');
