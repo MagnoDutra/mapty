5ee0aad3e1fa0ad4aeec21125984b5f2a8371620
MagnoDutra <jmagno.dutra@hotmail.com>
2023-08-04 22:47:11 -0300

    Evento de click no exercicio funcionando

--- script.js
+++ script.js
@@ -75,6 +75,7 @@ class App {
     form.addEventListener('submit', this._newWorkout.bind(this));
 
     inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -165,7 +166,6 @@ class App {
       workout = new Cycling(distance, duration, [lat, lng], elevation);
     }
 
-    console.log(workout);
     this.#workouts.push(workout);
 
     // Display marker
@@ -245,6 +245,15 @@ class App {
     }
 
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(wk => wk.id === workoutEl.dataset.id);
+    console.log(workout);
   }
 }
 
